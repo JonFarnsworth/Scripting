4e4e2617a2509b2bc16a6484a90ba7ba730de010
theeprofessor <thestinger44@hotmail.com>
2016-01-29 17:50:12 +0000

    New Updates

--- Lab 1/showsetidfiles.sh
+++ Lab 1/showsetidfiles.sh
@@ -3,9 +3,8 @@
 
 echo "Setuid file sin /usr"
 echo "==============="
-find /usr -perm -4000 -type f -ls
+find /usr -perm -4000 -type f -ls 
 
 echo "Setgid file sin /usr"
 echo "====================="
 find /usr -perm -2000 -type f -ls
-
