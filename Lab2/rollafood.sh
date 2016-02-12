8e83577807cc29de42825ff51408938e8c755170
theeprofessor <thestinger44@hotmail.com>
2016-02-12 18:13:29 +0000

    New Updates

--- Lab2/rollafood.sh
+++ Lab2/rollafood.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-
-
-
-
+# Array of foods
+foods=("Apple" "Banana" "Peach" "Grape" "Melon" "Squash" "Pizza" "Ice Cream" "Tacos" "Turkey" "Fish")
 # MAIN
 ################
-read -p "Pick two numbers between 1-6: " x y
+die1=$(($RANDOM % 6 + 1))
+die2=$(($RANDOM % 6 + 1))
+diceroll=$(($die1 + $die2))
+index=$(($diceroll - 2))
+echo "We chose a ${foods[$index]} because we rolled a $die1 and a $die2 for a $diceroll"
--- Lab2/showinterfaces.sh
+++ Lab2/showinterfaces.sh
@@ -6,5 +6,5 @@
 # MAIN
 ##############
 ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
-ifconfig lo | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
+
 route -n | awk '{ $1=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; print}' | sed '3!d'
