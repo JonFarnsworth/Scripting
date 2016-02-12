0c4f750f377b77bf06db66323041da22cffa098b
theeprofessor <thestinger44@hotmail.com>
2016-02-12 18:41:04 +0000

    New Updates

--- Lab2/showinterfaces.sh
+++ Lab2/showinterfaces.sh
@@ -10,6 +10,6 @@ interfaces=('ifconfig | grep '^[A-Za-z]' | awk "{print $1}"')
 eth0=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 echo "The ethernet connection has the address of $eth0"
 lo=$(ifconfig lo | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-echo "The loopback address of $lo"
+echo "The loopback address is $lo"
 route=$(route -n | awk '{$1=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; print}' | sed '3!d')
 echo "The default gateway has the address of $route"
